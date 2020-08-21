d88f1165440b0b3358a0f8b9b8c6c1cca9db4529
Tietohallinto <tietohallinto@BC-minimac050.local>
2020-08-21 12:40:49 +0300

    margin comment

--- js/inject.js
+++ js/inject.js
@@ -5,11 +5,11 @@ document.getElementById("slogan").innerHTML = "we hate our customers";
 document.getElementById("all").style.color = "#c7c3c3";
 document.getElementById("all").style.fontFamily = "Impact";
 
-document.getElementsByClassName("box py-4").style.margin = "10px 20px";
-document.getElementsByClassName("box py-4").style.boxShadow = "2px 2px 10px";
+// document.getElementsByClassName("box py-4").style.margin = "10px 20px";
+// document.getElementsByClassName("box py-4").style.boxShadow = "2px 2px 10px";
 
-document.getElementsByClassName("container").style.margin = "10px 20px";
-document.getElementsByClassName("container").style.boxShadow = "2px 2px 10px";
+// document.getElementsByClassName("container").style.margin = "10px 20px";
+// document.getElementsByClassName("container").style.boxShadow = "2px 2px 10px";
 
 const newNavitem = document.createElement("li");
 newNavitem.className = "list-inline-item";
