12fe71bd7a44fdb7eda8ddb847d807021a3fb3d9
Tietohallinto <tietohallinto@BC-minimac050.local>
2020-08-21 11:13:24 +0300

    margina nd box-shadow

--- js/inject.js
+++ js/inject.js
@@ -5,5 +5,6 @@ document.getElementById("slogan").innerHTML = "we hate our customers";
 document.getElementById("all").style.color = "#c7c3c3";
 document.getElementById("all").style.fontFamily = "Impact";
 
-document.getElementsByClassName("item").style.margin = "10px 20px";
-document.getElementsByClassName("item").style.boxShadow = "2px 2px 10px";
+document.getElementsByClassName("owl-item active").style.margin = "10px 20px";
+document.getElementsByClassName("owl-item active").style.boxShadow =
+  "2px 2px 10px";
