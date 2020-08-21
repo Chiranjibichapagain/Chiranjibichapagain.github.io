eb30447ef455a30cc6af8891687d06d1d97c215c
Tietohallinto <tietohallinto@BC-minimac050.local>
2020-08-21 10:40:26 +0300

    font changed

--- js/inject.js
+++ js/inject.js
@@ -3,3 +3,5 @@ console.log("hello world");
 document.getElementById("slogan").innerHTML = "we hate our customers";
 
 document.getElementById("all").style.color = "#c7c3c3";
+document.getElementById("all").style.fontFamily =
+  "Impact,Charcoal,sans-serif";
